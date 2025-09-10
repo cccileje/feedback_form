98b2ca98f634b3262661b53a65175d6e1f1918d9
cccileje <cccileje@gmail.com>
2025-09-10 11:50:46 -0400

    initialize states and implement change handlers

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -3,12 +3,26 @@ import './FeedbackForm.css'; // Import CSS for styling
 
 const FeedbackForm = () => {
 
-  return (
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        feedback: ''
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    }
+
+    return (
     <>
     <nav>
     Tell Us What You Think
     </nav>
-      <form className="feedback-form">
+        <form className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
         
@@ -28,9 +42,9 @@ const FeedbackForm = () => {
         ></textarea>
         <button type='submit'>Submit Feedback</button>
 
-      </form>
+        </form>
     </>
-  );
+    );
 };
 
 export default FeedbackForm;
